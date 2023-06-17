70813056688329466a620a18379418902399d415
RinshaRahees <rrahees@millenniumci.com>
2023-06-17 14:45:35 +0530

    Bot conversation animation done

--- selfonboarding/src/Homepage.js
+++ selfonboarding/src/Homepage.js
@@ -1,10 +1,24 @@
 import React from 'react'
 import './Homepage.css'
+import Header from './Header'
+import DVLogo from './DVLogo'
 
 function Homepage() {
   return (
     <div className='homepage'>
-        
+      <Header/>
+
+      <div className='animation_container'>
+
+        <img className='bot' src='https://webchat-qa.dialogview.ai/assets/bot-gif.gif' alt=''/>
+
+        <div className='bot_conversation_container'>
+          <span className='bot_convo'><p>Hello! Happy to see you here.</p></span>
+          <span className='bot_convo convo2_hide'><p>Let me take you through the key features of DialogView.</p></span>
+        </div>
+      
+      </div>
+
     </div>
   )
 }
