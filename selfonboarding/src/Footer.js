27fd3bb8a744a160cc9a351827499f56274593ff
RinshaRahees <rrahees@millenniumci.com>
2023-06-17 21:36:47 +0530

    Feature desc completed

---
+++ selfonboarding/src/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Footer.css'
+
+function Footer() {
+  return (
+    <div className='footer'>
+        <div className='footer_tagline'>
+            <p>Streamline Your Communications, <br/>Elevate Your Customer Experience</p>
+        </div>
+        <div className='footer_contact'>
+            <p>Abouts</p>
+        </div>
+    </div>
+  )
+}
+
+export default Footer
--- selfonboarding/src/Homepage.js
+++ selfonboarding/src/Homepage.js
@@ -2,13 +2,17 @@ import React from 'react'
 import './Homepage.css'
 import Header from './Header'
 import BotAnimation from './BotAnimation'
+import FeaturesDesc from './FeaturesDesc'
+import Footer from './Footer'
 
 function Homepage() {
   return (
     <div className='homepage'>
-      
+
       <Header/>
       <BotAnimation/>
+      <FeaturesDesc/>
+      {/* <Footer/> */}
 
     </div>
   )
