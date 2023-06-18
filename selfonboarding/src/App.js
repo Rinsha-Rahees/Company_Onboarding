08737829a1e39ff253a4fb39a8f5554b14a17021
RinshaRahees <rrahees@millenniumci.com>
2023-06-19 00:49:29 +0530

    Routing done button click routing not done

--- selfonboarding/src/App.js
+++ selfonboarding/src/App.js
@@ -3,14 +3,17 @@ import Homepage from './Homepage';
 import Form from './Form';
 import DVLogo from './DVLogo';
 import CopyrightFooter from './CopyrightFooter';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="app">
-      {/* <Homepage/> */}
-      <DVLogo/>
-      <Form/>
-      <CopyrightFooter/>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Homepage/>} />
+          <Route path='/registration' element={ [<DVLogo/>, <Form/>, <CopyrightFooter/>] } />
+        </Routes>
+      </Router>
     </div>
   );
 }
