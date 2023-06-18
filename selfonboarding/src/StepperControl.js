473d0d404235a2b154119e0db07f4b9eb6b77192
RinshaRahees <rrahees@millenniumci.com>
2023-06-19 00:21:39 +0530

    UI completed

--- selfonboarding/src/StepperControl.js
+++ selfonboarding/src/StepperControl.js
@@ -8,7 +8,7 @@ const StepperControl = ({ handleClick, currentStep, steps }) => {
 
       <button
         onClick={() => handleClick()} 
-        className={`relative bg-white text-slate-400 uppercase py-2 px-4
+        className={`relative right-56 bg-white text-slate-400 uppercase py-2 px-4
         rounded-xl font-semibold cursor-pointer border-2 border-slate-300
         hover:bg-slate-700 hover:text-white transition duration-200 ease-in-out
         ${currentStep === 1 ? "opacity-50 cursor-not-allowed" : " "} `}>
@@ -18,7 +18,7 @@ const StepperControl = ({ handleClick, currentStep, steps }) => {
         {/* Next Btn */}
       <button
         onClick={() => handleClick("next")}  
-        className="relative bg-dv-grey text-white uppercase py-2 px-4
+        className="relative left-56 bg-dv-grey text-white uppercase py-2 px-4
         rounded-xl font-semibold cursor-pointer hover:bg-dv-orange hover:text-white transition duration-200 ease-in-out">
         {currentStep === steps.length ? "Confirm" : "Next" }
       </button>
--- selfonboarding/src/steps/SubscriptionCheck.js
+++ selfonboarding/src/steps/SubscriptionCheck.js
@@ -12,26 +12,42 @@ function SubscriptionCheck() {
   return (
     <div className='subscriptionCheck'>
 
-      <div className="contactCard">
-        <h1>Contact Center</h1>
-      </div>
-      <div className="conferenceCard">
-
-      </div>
-
-      {/* 
-      
-      <button1 
-          onChange={handleChange}
+      <div className="subscription_container">
+        <div className="contactCard">
+        <span className='subscription_head'>Contact Center</span>
+        <ul className="product_features">
+          <li>Live Web Chat</li>
+          <li>VoIP Integration</li>
+          <li>AI-Powered Virtual Agent</li>
+          <li>Dashboard Analytics</li>
+          <li>Audio/Video Call</li>
+          <li>Screen Share</li>
+          <li>Omnichannel Messaging</li>
+          <li>Reminders & To-Do Lists</li>
+          <li>Canned Response</li>
+        </ul>
+        <button className="subscribe_btn" onChange={handleChange}
           value={userData["contactCenterSub"]}
-          name="contactCenterSub"/>
-
-      <button2
-          onChange={handleChange}
+          name="contactCenterSub" >Subscribe</button>
+        </div>
+        <div className="conferenceCard">
+        <span className='subscription_head'>Conference Solution</span>
+        <ul className="product_features">
+            <li>Live Web Chat</li>
+            <li>VoIP Integration</li>
+            <li>AI-Powered Virtual Agent</li>
+            <li>Dashboard Analytics</li>
+            <li>Audio/Video Call</li>
+            <li>Screen Share</li>
+            <li>Omnichannel Messaging</li>
+            <li>Reminders & To-Do Lists</li>
+            <li>Canned Response</li>
+        </ul>
+        <button className="subscribe_btn" onChange={handleChange}
           value={userData["conferenceSub"]}
-          name="conferenceSub"/>
-
-      */}
+          name="conferenceSub">Subscribe</button>
+        </div>
+      </div>
 
     </div>
   )
