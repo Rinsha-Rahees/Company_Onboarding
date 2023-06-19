42ba02388335d198277c8a298d730ec05a4b5b08
RinshaRahees <rrahees@millenniumci.com>
2023-06-19 16:05:16 +0530

    Logo issue

--- selfonboarding/src/DVLogo.js
+++ selfonboarding/src/DVLogo.js
@@ -5,8 +5,7 @@ function DVLogo() {
   return (
     <div className='dvlogo'>
       <div className="logo_header">
-        <a href='/'><img className='DVlogo' src='http://dialogview.millenniumci.net/wp-content/uploads/2023/05/dialogview-contact-center-solution.png'
-        alt=''/></a>
+        <a href='/'><img className='DVlogo' src='/DVLOGO.png' alt=''/></a>
       </div>
     </div>
   )
--- selfonboarding/src/Header.js
+++ selfonboarding/src/Header.js
@@ -4,8 +4,7 @@ import './Header.css'
 function Header() {
   return (
     <div className='header'>
-        <a href='/'><img className='dv_logo' src='http://dialogview.millenniumci.net/wp-content/uploads/2023/05/dialogview-contact-center-solution.png'
-        alt=''/></a>
+        <a href='/'><img className='dv_logo' src='/DVLOGO.png' alt=''/></a>
 
         <button className='startBtn'><span><a href='/registration'>Get Started</a></span></button>
     </div>
