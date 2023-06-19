2627ae6f0661a41dad814fc9426d4ba1b065beb4
RinshaRahees <rrahees@millenniumci.com>
2023-06-19 15:54:27 +0530

    Updated

--- selfonboarding/src/DVLogo.js
+++ selfonboarding/src/DVLogo.js
@@ -5,7 +5,7 @@ function DVLogo() {
   return (
     <div className='dvlogo'>
       <div className="logo_header">
-        <a href='http://localhost:3000/'><img className='DVlogo' src='http://dialogview.millenniumci.net/wp-content/uploads/2023/05/dialogview-contact-center-solution.png'
+        <a href='http://localhost:3000/'><img className='DVlogo' src='https://dialogview.millenniumci.net/wp-content/uploads/2023/05/dialogview-contact-center-solution.png'
         alt=''/></a>
       </div>
     </div>
--- selfonboarding/src/Header.js
+++ selfonboarding/src/Header.js
@@ -4,7 +4,7 @@ import './Header.css'
 function Header() {
   return (
     <div className='header'>
-        <a href='/'><img className='dv_logo' src='http://dialogview.millenniumci.net/wp-content/uploads/2023/05/dialogview-contact-center-solution.png'
+        <a href='/'><img className='dv_logo' src='https://dialogview.millenniumci.net/wp-content/uploads/2023/05/dialogview-contact-center-solution.png'
         alt=''/></a>
 
         <button className='startBtn'><span><a href='/registration'>Get Started</a></span></button>
