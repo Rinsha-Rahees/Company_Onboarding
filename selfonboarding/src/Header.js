4d460382d35ab82080356a18be6d8ca3fd3f92d5
RinshaRahees <rrahees@millenniumci.com>
2023-06-19 15:25:43 +0530

    Final UI

--- selfonboarding/src/Header.js
+++ selfonboarding/src/Header.js
@@ -2,18 +2,12 @@ import React from 'react'
 import './Header.css'
 
 function Header() {
-
-
-  const loadReg = () => {
-    // window.location.assign = 'http://www.google.com';
-  }
-
   return (
     <div className='header'>
         <a href='http://localhost:3000/'><img className='dv_logo' src='http://dialogview.millenniumci.net/wp-content/uploads/2023/05/dialogview-contact-center-solution.png'
         alt=''/></a>
 
-        <button className='startBtn'><span><a href='http://localhost:3000/registration'>Get Started</a></span></button>
+        <button className='startBtn'><span><a href='/registration'>Get Started</a></span></button>
     </div>
   )
 }
